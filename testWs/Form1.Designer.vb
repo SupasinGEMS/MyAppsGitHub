d0add832c65513f05ca6287628af133cd2485821
SupasinGEMS <gemslua@gmail.com>
2018-05-17 19:11:24 +0700

    CreateProject

---
+++ testWs/Form1.Designer.vb
@@ -0,0 +1,224 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnTestFunction = New System.Windows.Forms.Button()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.txtuser = New System.Windows.Forms.TextBox()
+        Me.txtpass = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnHello = New System.Windows.Forms.Button()
+        Me.btncustomerG = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnTestFunction
+        '
+        Me.btnTestFunction.Location = New System.Drawing.Point(291, 26)
+        Me.btnTestFunction.Name = "btnTestFunction"
+        Me.btnTestFunction.Size = New System.Drawing.Size(133, 23)
+        Me.btnTestFunction.TabIndex = 0
+        Me.btnTestFunction.Text = "TestFunction"
+        Me.btnTestFunction.UseVisualStyleBackColor = True
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(124, 55)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 1
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'txtuser
+        '
+        Me.txtuser.Location = New System.Drawing.Point(18, 28)
+        Me.txtuser.Name = "txtuser"
+        Me.txtuser.Size = New System.Drawing.Size(100, 20)
+        Me.txtuser.TabIndex = 2
+        '
+        'txtpass
+        '
+        Me.txtpass.Location = New System.Drawing.Point(18, 57)
+        Me.txtpass.Name = "txtpass"
+        Me.txtpass.Size = New System.Drawing.Size(100, 20)
+        Me.txtpass.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtuser)
+        Me.GroupBox1.Controls.Add(Me.txtpass)
+        Me.GroupBox1.Controls.Add(Me.btnLogin)
+        Me.GroupBox1.Location = New System.Drawing.Point(70, 144)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(206, 100)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "LoginUser"
+        '
+        'btnHello
+        '
+        Me.btnHello.Location = New System.Drawing.Point(349, 80)
+        Me.btnHello.Name = "btnHello"
+        Me.btnHello.Size = New System.Drawing.Size(75, 23)
+        Me.btnHello.TabIndex = 6
+        Me.btnHello.Text = "Hello"
+        Me.btnHello.UseVisualStyleBackColor = True
+        '
+        'btncustomerG
+        '
+        Me.btncustomerG.Location = New System.Drawing.Point(349, 119)
+        Me.btncustomerG.Name = "btncustomerG"
+        Me.btncustomerG.Size = New System.Drawing.Size(90, 23)
+        Me.btncustomerG.TabIndex = 7
+        Me.btncustomerG.Text = "CustomerGet"
+        Me.btncustomerG.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(379, 234)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Hello"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(150, 47)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "ConvertDate"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(845, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 10
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(845, 58)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(845, 84)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(786, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 13)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "รวมสุทธิ"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(786, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "รับเงิน"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(786, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "เงินทอน"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(478, 32)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(241, 142)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1165, 294)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btncustomerG)
+        Me.Controls.Add(Me.btnHello)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnTestFunction)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnTestFunction As Button
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents txtuser As TextBox
+    Friend WithEvents txtpass As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnHello As Button
+    Friend WithEvents btncustomerG As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ testWs/Form2.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnSaveTest = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtToString = New System.Windows.Forms.TextBox()
+        Me.txtStringBudder = New System.Windows.Forms.TextBox()
+        Me.btnToString = New System.Windows.Forms.Button()
+        Me.btnStringBudder = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(177, 211)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 0
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'btnSaveTest
+        '
+        Me.btnSaveTest.Location = New System.Drawing.Point(12, 197)
+        Me.btnSaveTest.Name = "btnSaveTest"
+        Me.btnSaveTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnSaveTest.TabIndex = 2
+        Me.btnSaveTest.Text = "SaveTest"
+        Me.btnSaveTest.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(142, 168)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 37)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "testMathRoud"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 171)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'txtToString
+        '
+        Me.txtToString.Location = New System.Drawing.Point(341, 12)
+        Me.txtToString.Multiline = True
+        Me.txtToString.Name = "txtToString"
+        Me.txtToString.Size = New System.Drawing.Size(138, 118)
+        Me.txtToString.TabIndex = 5
+        '
+        'txtStringBudder
+        '
+        Me.txtStringBudder.Location = New System.Drawing.Point(485, 12)
+        Me.txtStringBudder.Multiline = True
+        Me.txtStringBudder.Name = "txtStringBudder"
+        Me.txtStringBudder.Size = New System.Drawing.Size(138, 118)
+        Me.txtStringBudder.TabIndex = 6
+        '
+        'btnToString
+        '
+        Me.btnToString.Location = New System.Drawing.Point(341, 136)
+        Me.btnToString.Name = "btnToString"
+        Me.btnToString.Size = New System.Drawing.Size(110, 37)
+        Me.btnToString.TabIndex = 7
+        Me.btnToString.Text = "ToString"
+        Me.btnToString.UseVisualStyleBackColor = True
+        '
+        'btnStringBudder
+        '
+        Me.btnStringBudder.Location = New System.Drawing.Point(341, 179)
+        Me.btnStringBudder.Name = "btnStringBudder"
+        Me.btnStringBudder.Size = New System.Drawing.Size(110, 37)
+        Me.btnStringBudder.TabIndex = 8
+        Me.btnStringBudder.Text = "StringBudder"
+        Me.btnStringBudder.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(772, 348)
+        Me.Controls.Add(Me.btnStringBudder)
+        Me.Controls.Add(Me.btnToString)
+        Me.Controls.Add(Me.txtStringBudder)
+        Me.Controls.Add(Me.txtToString)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnSaveTest)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnOK)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOK As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnSaveTest As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtToString As TextBox
+    Friend WithEvents txtStringBudder As TextBox
+    Friend WithEvents btnToString As Button
+    Friend WithEvents btnStringBudder As Button
+End Class
